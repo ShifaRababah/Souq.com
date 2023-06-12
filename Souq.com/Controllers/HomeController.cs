77797839482748b7a5fed268b01ff193137e21fa
Shifa Rababah <shifa.rababaah@gmail.com>
2023-06-12 22:37:08 +0300

    Add contact and review section

--- Souq.com/Controllers/HomeController.cs
+++ Souq.com/Controllers/HomeController.cs
@@ -28,7 +28,49 @@ namespace Souq.com.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult GetProductsByCategory(int id)
+        {
+            var products= _context.Products.Where(x=>x.CategoryId==id).ToList();
+            return View(products);
+        }
+        [HttpGet]
+        public IActionResult ProductSearch(string xname)
+        {
+            var products = _context.Products.Where(x=>x.Name.Contains(xname)).ToList();
+            return View(products);
+        }
+
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult SendReview(Review review)
+        {
+            _context.Reviews.Add(new Review
+            {
+                Name= review.Name,  
+                Subject=review.Subject,
+                Message=review.Message, 
+                Email=review.Email, 
+            });
+            _context.SaveChanges();
+            return RedirectToAction("Index");   
+        }
+
+        public IActionResult Reviews()
+        {
+            var reviews = _context.Reviews.ToList();
+
+            return View(reviews);
+
+        }
+
+
+
+
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- Souq.com/Models/Review.cs
+++ Souq.com/Models/Review.cs
@@ -3,7 +3,7 @@
     public class Review
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string ? Name { get; set; }
         public string Email { get; set; }
         public string Subject { get; set; }   
         public string Message { get; set; }   
